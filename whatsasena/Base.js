e2f422e1f3f9d95ba4f5eb11aaf9c47473161cb3
BlackVampired ✅ <91042717+BlackVampired@users.noreply.github.com>
2021-11-22 18:53:11 -0800

    Add files via upload

--- whatsasena/Base.js
+++ whatsasena/Base.js
@@ -1,1 +1,24 @@
+/* Copyright (C) 2021 AmdA.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Amdibell - AmdA
+*/
+
+'use strict';
+
+class Base {
+    constructor(client) {
+        Object.defineProperty(this, 'client', { value: client });
+    }
+
+    _clone() {
+        return Object.assign(Object.create(this), this);
+    }
+    
+    _patch(data) { return data; }
+}
+
+module.exports = Base;
 
---
+++ whatsasena/index.js
@@ -0,0 +1,16 @@
+/* Copyright (C) 2021 AmdA.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Amdibell - AmdA
+*/
+
+module.exports = {
+    Base: require('./Base'),
+    Message: require('./Message'),
+    StringSession: require('./StringSession'),
+    ReplyMessage: require('./ReplyMessage'),
+    Image: require('./Image'),
+    Video: require('./Video')
+};
